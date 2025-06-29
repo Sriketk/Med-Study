358bfc3ff517754fce1bb128e88dd9bada474dff
Sriket Komali <sriketk5@gmail.com>
2025-06-28 22:08:29 -0400

    connected to mongo db

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,7 +1,7 @@
-import { MongoClient, Db } from "mongodb";
+import { MongoClient, Db, ServerApiVersion } from "mongodb";
 
-const uri = process.env.MONGODB_URI;
-const dbName = process.env.MONGODB_DB;
+const uri = process.env.MONGODB_URI ?? "";
+const dbName = process.env.MONGODB_DB ?? "";
 
 if (!uri) {
   throw new Error("Please define the MONGODB_URI environment variable in .env.local");
@@ -17,10 +17,18 @@ export async function connectToDatabase() {
   if (cachedClient && cachedDb) {
     return { client: cachedClient, db: cachedDb };
   }
-  const client = new MongoClient(uri as string);
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
   await client.connect();
   const db = client.db(dbName as string);
   cachedClient = client;
   cachedDb = db;
+  console.log("Connected to MongoDB");
+  console.log(db)
   return { client, db };
 } 
