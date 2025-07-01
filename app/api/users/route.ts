869035727c4c15bb3d0c84c2c648681a543448fb
Sriket Komali <sriketk5@gmail.com>
2025-07-01 17:25:15 -0400

    FUCK MONGOOOSE

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,15 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
+import mongoose, { Schema, models } from "mongoose";
 import { connectToDatabase } from "@/lib/mongodb";
+
+
+let QbankSchema = new Schema({
+  topic: String,
+  subtopic: String,
+  question: String,
+  choices: [String],
+  answer: String,
+  explanation: String,
+  source: String,
+  created_at: String,
+  embedding: [Number],
+  // Add more fields as needed
+});
+
+let dumbSchema = new Schema({
+  name: String,
+});
+const dumb = models.dumb || mongoose.model("dumb", dumbSchema);
+const qbanks = models.qbanks || mongoose.model("qbanks", QbankSchema);
+
+// Force the colle
+// ction name to 'Qbank' to prevent pluralization
+// const dumb = models.dumb || mongoose.model("stup", dumbSchema);
+
+// const Qbank = models.dumb || mongoose.model("dumb", dumbSchema);
+const filter = {
+  name: "jhonn",
+};
+
+const qbanksFilter = {
+  topic: "Biochemistry",
+}
 
 export async function GET(request: NextRequest) {
   try {
-    const { db } = await connectToDatabase();
-    return NextResponse.json({ message: "Hello, world!" });
-    //   const user = await db.collection("users").findOne({ userId });
-    //   if (!user) {
-    //     return NextResponse.json({ error: "User not found" }, { status: 404 });
-    //   }
-    //   return NextResponse.json(user);
+    await connectToDatabase();
+    const result = await qbanks.findOne(qbanksFilter);
+    // const result = await dumb.find(filter);
+    return NextResponse.json(result);
   } catch (error) {
     return NextResponse.json(
       { error: "Database error", details: String(error) },
--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,34 +1,8 @@
 import { MongoClient, Db, ServerApiVersion } from "mongodb";
+import mongoose from "mongoose";
 
 const uri = process.env.MONGODB_URI ?? "";
-const dbName = process.env.MONGODB_DB ?? "";
-
-if (!uri) {
-  throw new Error("Please define the MONGODB_URI environment variable in .env.local");
-}
-if (!dbName) {
-  throw new Error("Please define the MONGODB_DB environment variable in .env.local");
-}
-
-let cachedClient: MongoClient | null = null;
-let cachedDb: Db | null = null;
 
 export async function connectToDatabase() {
-  if (cachedClient && cachedDb) {
-    return { client: cachedClient, db: cachedDb };
-  }
-  const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-  await client.connect();
-  const db = client.db(dbName as string);
-  cachedClient = client;
-  cachedDb = db;
-  console.log("Connected to MongoDB");
-  console.log(db)
-  return { client, db };
+  await mongoose.connect(uri);
 } 
