a9ae53080f2d92857d226086d46dae55d99b769b
Sriket Komali <sriketk5@gmail.com>
2025-07-03 13:35:10 -0400

    basic drizzle/better/supabase set ups

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,13 +1,22 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@/app/index"; // your drizzle instance
-import { usersTable } from "@/app/db/schema";
-
+import { db } from "@/app/db/auth/index"; // your drizzle instance
+ 
 export const auth = betterAuth({
-    database: drizzleAdapter(db, { 
-        schema: {
-            users: usersTable,
-        },
+    database: drizzleAdapter(db, {
         provider: "pg", // or "mysql", "sqlite"
-    })
+    }),
+    emailAndPassword: {  
+        enabled: true
+    },
+    socialProviders: { 
+        github: { 
+           clientId: process.env.GITHUB_CLIENT_ID as string, 
+           clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+        google: { 
+            clientId: process.env.GOOGLE_CLIENT_ID as string, 
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
+         }, 
+    },
 });
